f2169bfdf9fdfd0802cdd0b7d9b3b10fa524fad4
JahanzaibArifAwan <jahanzaibawan2008@gmail.com>
2026-01-02 19:54:26 +0500

    Resolved routing problem

--- src/AuthRoute.jsx
+++ src/AuthRoute.jsx
@@ -1,14 +1,13 @@
 import { useContext } from "react"
 import userContext from "./context/user"
-import { useNavigate } from "react-router-dom"
+import { Navigate } from "react-router-dom"
 
 const AuthRoute = ({children})=>{
     const {isLoggedIn, loading} = useContext(userContext)
-    const navigate = useNavigate()
     if(loading)
         return <></>
     else if(!isLoggedIn)
-        navigate("/login")
+        return <Navigate to="/login" replace/>
 
     return children
 }
